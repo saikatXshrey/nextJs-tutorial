28a1400b559d9e45ece7266caf78a198d1cba190
saikatXshrey <saikatxshrey@gmail.com>
2021-12-02 19:30:49 +0530

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return (
+        <footer>Copyright 2021 ninja</footer>
+    );
+}
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum impedit minus qui nesciunt non adipisci, sunt illo temporibus natus a ducimus suscipit, et ratione nostrum illum tempore repellat iusto itaque. Possimus accusantium, incidunt exercitationem rerum aspernatur ipsam nemo minima, voluptate odio eveniet assumenda distinctio deleniti autem fugiat repudiandae! Culpa, soluta.</p>
+            </div>
+        </>
+    );
+}
+
+export default About;
